9503402166fc519a3659e93d29123810f04daf9c
RD-Bhowmik <ronodeep.bhowmik@g.bracu.ac.bd>
2025-02-19 23:04:13 +0600

    day 2 3 added

---
+++ day2.py
@@ -0,0 +1,17 @@
+print("something somethign\nthis is something else and this does nothing/yes nothing at all")
+
+a = 2
+if (1 < a >= 2):
+    print("this is day 2")
+else: 
+    print("this is not day 2")
+    
+x = "check escape sequence" 
+
+if x == "check escape sequence":
+    print('\"check escape\' sequence \" working')
+else: 
+    print("maybe worked before")
+
+
+print("something",7,3454, sep="~" , end="00012\n")
